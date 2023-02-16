9c464a1bf961a1bd0d06b28009e7f86f4c90b713
kababii56 <laureenwambui2@gmail.com>
2023-02-16 13:06:02 -0800

    sixth

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main -Entry
+ * main -ENTRY
  * Return:Always 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
